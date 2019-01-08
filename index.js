68b93b706f8415463e3e96788692df92ef2e4dc9
edthomas93 <et1270@my.bristol.ac.uk>
2019-01-08 15:17:47 +0000

    added event listener for search button

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-alert("testing 123");
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+
+searchForm.addEventListener('submit', event => {
+  console.log("search event");
+  event.preventDefault(); //prevents form from submitting to a file
+})
