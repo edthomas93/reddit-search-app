00686838461bfdc71816af7441da310dcddce35b
edthomas93 <et1270@my.bristol.ac.uk>
2019-01-08 16:27:08 +0000

    all values with in form are recorded and error handle no search input

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 
 searchForm.addEventListener('submit', event => {
-  console.log("search event");
+  const searchTerm = searchInput.value;
+  const sortBy = document.querySelector('input[name="sortby"]:checked').value;
+  const searchLimit = document.getElementById('limit').value;
+  
+  if(searchTerm === ''){
+    showMessage('Please add search term', 'alert');
+  }
+
   event.preventDefault(); //prevents form from submitting to a file
 })
+
+function showMessage(message, className){
+  const div = document.createElement('div');
+  div.className = `${className}`;
+  div.appendChild(document.createTextNode(message));
+  const searchContainer = document.getElementById('search-container');
+  const search = document.getElementById('search');
+  searchContainer.insertBefore(div, search);
+};
